256767dd62751c3d2a19f7f80ee49401c84029bc
Uriel Carrera <a3520110324@alumno.uttehuacan.edu.mx>
2023-11-02 15:07:40 -0600

    V1-UHC-Modulo1:Diseño Sidebar

--- src/Global/Sidebar/Sidebar.jsx
+++ src/Global/Sidebar/Sidebar.jsx
@@ -1,75 +1,78 @@
-import { useEffect, useRef, useState } from 'react';
+import React, { useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import { List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
-import { AdminPanelSettings, ArrowBack, Assessment, CalendarToday,   DashboardSharp, MenuOutlined, PeopleAltSharp, Settings, Summarize } from '@mui/icons-material';
+import {
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+
+  IconButton,
+} from '@mui/material';
+import {
+  DashboardSharp,
+  PeopleAltSharp,
+  Summarize,
+  CalendarToday,
+  Assessment,
+  Settings,
+  AdminPanelSettings,
+  MenuOutlined,
+  ArrowBack,
+} from '@mui/icons-material';
 
 import "./Sidebar.css";
 import { AdminRoutes } from '../../Models/routes';
 
 const sidebarNavItems = [
-
   {
     display: 'Dashboard',
     icon: <DashboardSharp />,
     to: AdminRoutes.DASHBOARD,
-    section: 'dashboard'
   },
   {
     display: 'Empleados',
     icon: <PeopleAltSharp />,
     to: AdminRoutes.EMPLEADOS,
-    section: 'sucursales'
   },
   {
     display: 'Reportes',
     icon: <Summarize />,
     to: AdminRoutes.REPORTES,
-    section: 'reparacion'
   },
   {
     display: 'Vacaciones',
-    icon:  <CalendarToday />,
+    icon: <CalendarToday />,
     to: AdminRoutes.VACACIONES,
-    section: 'ventas'
   },
   {
-    display: 'Analisis ',
+    display: 'Analisis',
     icon: <Assessment />,
     to: AdminRoutes.ANALISIS,
-    section: 'inventario'
   },
   {
-    display : 'Configuracion',
-    icon : <Settings />,
-    to : AdminRoutes.CONFIGURACION,
-    section : 'configuracion'
-
+    display: 'Configuracion',
+    icon: <Settings />,
+    to: AdminRoutes.CONFIGURACION,
   },
   {
-    display : 'Usuarios',
-    icon : <AdminPanelSettings />,
-    to : AdminRoutes.USUARIOS,
-    section : 'usuarios'
+    display: 'Usuarios',
+    icon: <AdminPanelSettings />,
+    to: AdminRoutes.USUARIOS,
   },
   {
-    display : 'pagos',
-    icon : <Assessment />,
-    to : AdminRoutes.PAGOS,
-    section : 'Pagos'
-  }
-
+    display: 'Pagos',
+    icon: <Assessment />,
+    to: AdminRoutes.PAGOS,
+  },
 ];
 
 const Sidebar = () => {
-  const [sidebarVisible, setSidebarVisible] = useState(true);
-  const [activeIndex, setActiveIndex] = useState(0);
-  const sidebarRef = useRef();
   const location = useLocation();
+  const [sidebarVisible, setSidebarVisible] = React.useState(true);
+  const [activeItem, setActiveItem] = React.useState('');
 
   useEffect(() => {
-    const curPath = location.pathname.split('/')[1];
-    const activeItem = sidebarNavItems.findIndex(item => item.to === `/${curPath}`);
-    setActiveIndex(activeItem);
+    setActiveItem(location.pathname);
   }, [location]);
 
   const toggleSidebar = () => {
@@ -77,38 +80,38 @@ const Sidebar = () => {
   };
 
   return (
-    <div>
-
-   
-    <div className={`sidebar ${sidebarVisible ? 'visible' : 'hidden'}`}>
-    <div className="sidebar__toggle" onClick={toggleSidebar}>
-      <span className="material-icons">{sidebarVisible ? <ArrowBack></ArrowBack> : <MenuOutlined></MenuOutlined> }</span>
-    </div>
-
-     
-
-      <div className={`sidebar__content`}>
-        <div  className={` ${sidebarVisible ? 'sidebar__logo' : 'hiddenLogo'}`}>
-          <h3>Human Gestion</h3>
-        </div>
-
-        <div ref={sidebarRef} className="sidebar__menu">
+    <div style={{ background: '#02132c' }}>
+      <div
+       
+        className={`sidebar ${sidebarVisible ? 'visible' : 'hidden'}`}
+        style={{ background: '#02132c' }}
+      >
+        <IconButton onClick={toggleSidebar} style={{ borderRadius: '0', color:'white' }}>
+          {sidebarVisible ? <MenuOutlined /> : <ArrowBack />}
+        </IconButton>
+        <div className={`sidebar__content`}>
+          <div className={`${sidebarVisible ? 'sidebar__logo' : 'hiddenLogo'}`}>
+            <h3>Human Gestion</h3>
+          </div>
           <List>
-            {sidebarNavItems.map((item, index) => (
+            {sidebarNavItems.map((item) => (
               <ListItem
-                key={index}
+                key={item.to}
+             
                 component={Link}
                 to={item.to}
-                className={`sidebar__menu__item ${activeIndex === index ? 'active' : ''}`}
+                selected={item.to === activeItem}
+                className="sidebar__menu__item"
               >
-                <ListItemIcon className='sidebar_icons' style={{color:'#0f4391'}}>{item.icon}</ListItemIcon>
-                <ListItemText primary={item.display} className={` ${sidebarVisible ? 'sidebar_item_text' : 'hiddenLogo'}`} />
+                <ListItemIcon className="sidebar_icons" style={{ color: '#0f4391' }}>
+                  {item.icon}
+                </ListItemIcon>
+                <ListItemText primary={item.display} className={`${sidebarVisible ? 'sidebar_item_text' : 'hiddenLogo'}`} />
               </ListItem>
             ))}
           </List>
         </div>
       </div>
-    </div>
     </div>
   );
 };
