8fae04b382d66b36567d8639f107910be76c628d
Uriel Carrera <a3520110324@alumno.uttehuacan.edu.mx>
2023-11-19 14:35:49 -0600

    Refactor getEmpleados function and handle error cases

--- src/Pages/Admin/Empleados/Empleados.jsx
+++ src/Pages/Admin/Empleados/Empleados.jsx
@@ -6,205 +6,191 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import createData from "../../../Redux/states/Empleados"
+
 import './Empleados.css'
 import ControlPointIcon from '@mui/icons-material/ControlPoint';
 import IconButton from '@mui/material/IconButton';
-import React, {useState, useEffect} from 'react';
-import {Modal, ModalBody, ModalFooter,ModalHeader} from 'reactstrap';
+import  {useState, useEffect} from 'react';
+import {Modal, ModalBody, ModalHeader} from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, FormControl,InputLabel, Input, FormHelperText, 
+import {  FormControl,InputLabel, Input, FormHelperText, 
   Grid
 } from "@mui/material";
 import { getEmpleados } from '../../../Services/Empleados';
 
-const Empleados=()=>{
-   
-  
-  // const [checkedState, setCheckedState] = useState(1);
-  const [insertarEmpleado, setInsertarEmpleado]=useState
-  ({
-      name:'',
-      apellidoPa:'',
-      apellidoMa:'',
-      departamento: '', 
-      puesto:'',
-      edad:'',
-  })
-  const [data,setData]=useState([insertarEmpleado])
-  
-  const handleChange=e=>{
-    const {name, value}=e.target;
-    setInsertarEmpleado({
-      ...insertarEmpleado,
-      [name]:value
-    });
-  }
-  // const handleOnChange=e=>{
-  //   setCheckedState(e.target.value);
-  // }
+const Empleados = () => {
+  const [insertarEmpleado, setInsertarEmpleado] = useState({
+    id: '',
+    name: '',
+    apellidoPa: '',
+    apellidoMa: '',
+    departamento: '',
+    puesto: '',
+    edad: '',
+  });
+
+  const [data, setData] = useState([insertarEmpleado]);
+  const [modalAgregar, setModalAgregar] = useState(false);
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setInsertarEmpleado((prevState) => ({
+      ...prevState,
+      [id]: value,
+    }));
+    return insertarEmpleado;
+  };
 
   useEffect(() => {
-    getEmpleados().then((res) => {
-      console.log(res);
-      setData(res);
+    getEmpleados().then((storedData) => {
+      setData(storedData);
     });
-}, [data]);
+  }, []);
 
-  const[modalAgregar, setModalAgregar]=useState(false);
-  // const[modalEliminar, setModalEliminar]=useState(false);
-  const agregarEmpleado=()=>
-    {
-      const empleados = JSON.parse(localStorage.getItem('empleadosData'));
-      const nuevoEmpleado = { ...data, id: empleados.length + 1};
-      empleados.push(nuevoEmpleado);
-      localStorage.setItem('empleadosData', JSON.stringify(empleados))
-      .then(response=>{
-        setData(data.concat(response.data));
-        console.log(response)
-        alert("Em´pleado Registrado");
-        abrircerrarModalAgregar();
-      }).catch(error=>{
-        console.error(error.response.data)
-      })
-      
-    }
-    
-   
-    const abrircerrarModalAgregar=()=>{
-      setModalAgregar(!modalAgregar);
-    }
+  const agregarEmpleado = () => {
+    const empleados = JSON.parse(localStorage.getItem('empleadosData'));
+    const newEmpleado = { ...insertarEmpleado, id: empleados.length + 1 };
+  empleados.push(newEmpleado);
+  localStorage.setItem('empleadosData', JSON.stringify(empleados));
+  abrircerrarModalAgregar();
+  window.location.reload();
+  };
 
-    const seleccionarGestor=()=>{
-      abrircerrarModalAgregar();
-    }
+  const abrircerrarModalAgregar = () => {
+    setModalAgregar(!modalAgregar);
 
-    return (
-      <div>
-        <div className='button'>
-            <Button variant="contained" onClick={()=>seleccionarGestor()}>Agregar</Button>
-        </div>
-        <div className='table'>
+  };
+
+  const seleccionarGestor = () => {
+    abrircerrarModalAgregar();
+  };
+
+  return (
+    <div>
+      <div className="button">
+        <Button variant="contained" onClick={seleccionarGestor}>
+          Agregar
+        </Button>
+      </div>
+      <div className="table">
         <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell align="right">Nombre</TableCell>
-            <TableCell align="right">Apellido Paterno</TableCell>
-            <TableCell align="right">Apellido Materno</TableCell>
-            <TableCell align="right">Departamento</TableCell>
-            <TableCell align="right">Puesto</TableCell>
-            <TableCell align="right">Edad</TableCell>
-            
-            <TableCell align="center">Actions&nbsp;(g)</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.map((row) => (
-            <TableRow
-              key={row.id}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              {/* <TableCell component="th" scope="row">
-                {row.id}
-              </TableCell> */}
-              <TableCell align="right">{row.name}</TableCell>
-              <TableCell align="right">{row.apellidoPa}</TableCell>
-              <TableCell align="right">{row.apellidoMa}</TableCell>
-              <TableCell align="right">{row.departamento}</TableCell>
-              <TableCell align="right">{row.puesto}</TableCell>
-              <TableCell align="right">{row.edad}</TableCell>
-              <Button  size='small' variant="contained">Editar</Button>
-              <Button className='B' size='small' variant="contained">Eliminar</Button>
-              <IconButton>
-                <ControlPointIcon/>
-              </IconButton>
-              
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell align="right">Nombre</TableCell>
+                <TableCell align="right">Apellido Paterno</TableCell>
+                <TableCell align="right">Apellido Materno</TableCell>
+                <TableCell align="right">Departamento</TableCell>
+                <TableCell align="right">Puesto</TableCell>
+                <TableCell align="right">Edad</TableCell>
+                <TableCell align="center">Actions&nbsp;(g)</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {data.map((row) => (
+                <TableRow
+                  key={row.id}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell align="right">{row.name}</TableCell>
+                  <TableCell align="right">{row.apellidoPa}</TableCell>
+                  <TableCell align="right">{row.apellidoMa}</TableCell>
+                  <TableCell align="right">{row.departamento}</TableCell>
+                  <TableCell align="right">{row.puesto}</TableCell>
+                  <TableCell align="right">{row.edad}</TableCell>
+                  <Button size="small" variant="contained">
+                    Editar
+                  </Button>
+                  <Button className="B" size="small" variant="contained">
+                    Eliminar
+                  </Button>
+                  <IconButton>
+                    <ControlPointIcon />
+                  </IconButton>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
 
-    <Modal isOpen={modalAgregar}>
-      <ModalHeader>Agregar Empleado</ModalHeader>
-      <ModalBody>
-      <div>
-            <Grid container spacing={2}>
+        <Modal isOpen={modalAgregar}>
+          <ModalHeader>Agregar Empleado</ModalHeader>
+          <ModalBody>
+            <div>
+              <Grid container spacing={2}  >
                 <Grid item xs={6} md={5.5}>
-                    <FormControl>
-                        <InputLabel>Nombre(s)</InputLabel>
-                        <Input id="nombre" type="nombre" onChange={handleChange}/>
-                        <FormHelperText id="nombre">Solo de aceptan Mayusculas y minusculas</FormHelperText>
-                    </FormControl>
+                  <FormControl>
+                    <InputLabel>Nombre(s)</InputLabel>
+                    <Input id="name" type="nombre" onChange={handleChange} />
+                    <FormHelperText id="nombre">
+                      Solo se aceptan mayúsculas y minúsculas
+                    </FormHelperText>
+                  </FormControl>
                 </Grid>
                 <Grid item xs={6} md={6}>
-                    <FormControl>
-                        <InputLabel>Apellido Paterno</InputLabel>
-                        <Input id="paterno" type="paterno" onChange={handleChange}/>
-                        <FormHelperText id="paterno">Solo de aceptan Mayusculas y minusculas</FormHelperText>
-                    </FormControl>
+                  <FormControl>
+                    <InputLabel>Apellido Paterno</InputLabel>
+                    <Input id="apellidoPa" type="paterno" onChange={handleChange} />
+                    <FormHelperText id="paterno">
+                      Solo se aceptan mayúsculas y minúsculas
+                    </FormHelperText>
+                  </FormControl>
                 </Grid>
                 <Grid item xs={6} md={5.5}>
-                    <FormControl>
-                        <InputLabel>Apellido Materno</InputLabel>
-                        <Input id="maternio" type="materno" onChange={handleChange}/>
-                        <FormHelperText id="nombre">Solo de aceptan Mayusculas y minusculas</FormHelperText>
-                    </FormControl>
-                </Grid>
-                {/* <Grid item xs={6} md={6}>
-                    <FormControl>
-                        <InputLabel>Estatus</InputLabel>
-                        <Input id="estatus" type="estatus" onChange={handleChange}/>
-                        <FormHelperText id="estatus">Solo de aceptan Mayusculas y minusculas</FormHelperText>
-                    </FormControl>
-                </Grid>
-                <Grid item xs={6} md={5.5}>
-                    <FormControl>
-                        <InputLabel>RFC</InputLabel>
-                        <Input id="rfc" type="rfc" onChange={handleChange}/>
-                        <FormHelperText id="rfc">Mayusculas, minusculas y numeros</FormHelperText>
-                    </FormControl>
-                </Grid> */}
-                <Grid item xs={6} md={6}>
-                    <FormControl>
-                        <InputLabel>Departamento</InputLabel>
-                        <Input id="departamento" type="departamento" onChange={handleChange}/>
-                        <FormHelperText id="departamento">Solo de aceptan Mayusculas y minusculas</FormHelperText>
-                    </FormControl>
-                </Grid>
-                <Grid item xs={6} md={5.5}>
-                    <FormControl>
-                        <InputLabel>Puesto</InputLabel>
-                        <Input id="puesto" type="puesto" onChange={handleChange}/>
-                        <FormHelperText id="puesto">Solo de aceptan Mayusculas y minusculas</FormHelperText>
-                    </FormControl>
+                  <FormControl>
+                    <InputLabel>Apellido Materno</InputLabel>
+                    <Input id="apellidoMa" type="materno" onChange={handleChange} />
+                    <FormHelperText id="nombre">
+                      Solo se aceptan mayúsculas y minúsculas
+                    </FormHelperText>
+                  </FormControl>
                 </Grid>
                 <Grid item xs={6} md={6}>
-                    <FormControl>
-                        <InputLabel>Edad</InputLabel>
-                        <Input id="edad" type="edad" onChange={handleChange}/>
-                        <FormHelperText id="edad">Solo de aceptan numeros</FormHelperText>
-                    </FormControl>
+                  <FormControl>
+                    <InputLabel>Departamento</InputLabel>
+                    <Input id="departamento" type="departamento" onChange={handleChange} />
+                    <FormHelperText id="departamento">
+                      Solo se aceptan mayúsculas y minúsculas
+                    </FormHelperText>
+                  </FormControl>
                 </Grid>
                 <Grid item xs={6} md={5.5}>
-                    <Button variant="contained" color="primary"
-                    onClick={()=>agregarEmpleado()}>
-                        Registrar
-                    </Button>
+                  <FormControl>
+                    <InputLabel>Puesto</InputLabel>
+                    <Input id="puesto" type="puesto" onChange={handleChange} />
+                    <FormHelperText id="puesto">
+                      Solo se aceptan mayúsculas y minúsculas
+                    </FormHelperText>
+                  </FormControl>
                 </Grid>
                 <Grid item xs={6} md={6}>
-                    <Button variant="contained" color="error" onClick={()=>abrircerrarModalAgregar()}>
-                        Cancelar
-                    </Button>
+                  <FormControl>
+                    <InputLabel>Edad</InputLabel>
+                    <Input id="edad" type="edad" onChange={handleChange} />
+                    <FormHelperText id="edad">Solo se aceptan números</FormHelperText>
+                  </FormControl>
                 </Grid>
-            </Grid> 
+                <Grid item xs={6} md={5.5}>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={() => agregarEmpleado()}
+                  >
+                    Registrar
+                  </Button>
+                </Grid>
+                <Grid item xs={6} md={6}>
+                  <Button variant="contained" color="error"  type onClick={abrircerrarModalAgregar}>
+                    Cancelar
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </ModalBody>
+        </Modal>
+      </div>
     </div>
-      </ModalBody>
-    </Modal>
-        </div>
-    </div>
-    )
-}
+  );
+};
 
 export default Empleados;
--- src/Services/Empleados.ts
+++ src/Services/Empleados.ts
@@ -29,9 +29,46 @@ const empleadosData = [
     }
 ];
 
-export function getEmpleados() {
-    return fetch ('http://localhost:3000/empleados')
-    .then(response => response.json())
-    .then(response => response)
-    .catch(err => empleadosData);
+
+
+function getOrCreateEmpleadosData(): Promise<Array<any>> {
+    return new Promise((resolve) => {
+      let storedData: Array<any> | null = JSON.parse(localStorage.getItem('empleadosData') || 'null');
+  
+      if (!storedData) {
+        const defaultData = 
+        [
+            {
+                id: 0,
+                name : '',
+                apellidoPa:'',
+                apellidoMa:'',
+                departamento: '',
+                puesto: '',
+                edad : '',
+            }
+        ];
+        localStorage.setItem('empleadosData', JSON.stringify(defaultData));
+        storedData = defaultData;
+      }
+  
+      resolve(storedData);
+    });
+  }
+  
+export async  function getEmpleados() {
+  try {
+        const response = await fetch('http://localhost:3000/empleados');
+        const data = await response.json();
+        return data;
+    
+  }
+    catch (error) {
+        const storedData: Array<any> = await getOrCreateEmpleadosData();
+        const combinedArray = [...empleadosData, ...storedData];
+        return combinedArray;
+    }
 };
+
+
+
