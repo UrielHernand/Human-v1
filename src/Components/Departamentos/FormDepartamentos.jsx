53389373714ecb324bb04748a23695c5553b8910
Uriel Carrera <a3520110324@alumno.uttehuacan.edu.mx>
2023-11-22 00:27:32 -0600

    Fix formatting and add new features to FormDepartamentos component

--- src/Components/Departamentos/FormDepartamentos.jsx
+++ src/Components/Departamentos/FormDepartamentos.jsx
@@ -1,17 +1,16 @@
-import  { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import {
   Button,
   Grid,
   TextField,
   Typography,
- 
 } from "@mui/material";
 import {
   AccountCircle,
+  AttractionsOutlined,
   Description,
   Email,
   Image,
-
   Phone,
   RoomService,
 } from "@mui/icons-material";
@@ -26,14 +25,14 @@ const FormDepartamentos = ({ action, data }) => {
 
   const [modo] = useState(action === Typeaction.register ? 'registrar' : 'actualizar');
   const [formData, setFormData] = useState({
-   name : '',
-    description : '',
-  phone : '',
-  email : '',
-  image : '',
-  services : [],
-
-   
+    name: '',
+    description: '',
+    phone: '',
+    email: '',
+    image: '',
+    state: true,
+    services: [''], // Agregado un servicio por defecto
+    id: null, // Establecer id como null por defecto
   });
 
   useEffect(() => {
@@ -61,23 +60,32 @@ const FormDepartamentos = ({ action, data }) => {
     });
   };
 
-  const getStartAdornment = (campo) => {
-    switch (campo) {
-      case 'name':
-        return <AccountCircle />;
-      case 'description':
-        return <Description />;
-      case 'phone':
-        return <Phone />;
-      case 'image':
-        return <Image />;
-      case 'email':
-        return <Email />;
-      case 'services':
-        return <RoomService />;
-      default:
-        return null;
-    }
+  const handleAddService = () => {
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      services: [...prevFormData.services, ''], // Agrega un servicio vacío al array
+    }));
+  };
+
+  const handleRemoveService = (index) => {
+    setFormData((prevFormData) => {
+      const updatedServices = [...prevFormData.services];
+      updatedServices.splice(index, 1); // Elimina el servicio en el índice proporcionado
+      return {
+        ...prevFormData,
+        services: updatedServices,
+      };
+    });
+  };
+
+  const handleServiceChange = (event, index) => {
+    const updatedServices = [...formData.services];
+    updatedServices[index] = event.target.value;
+
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      services: updatedServices,
+    }));
   };
 
   const handleSubmit = (event) => {
@@ -88,19 +96,157 @@ const FormDepartamentos = ({ action, data }) => {
     if (modo === 'registrar') {
       console.log('entro');
 
-      const departamentos = JSON.parse(localStorage.getItem('departamentosData')) || [];
+      const departamentos = JSON.parse(localStorage.getItem('departamentosData')) ;
       const nuevoDepartamento = { ...formData, id: departamentos.length + 1 };
       departamentos.push(nuevoDepartamento);
       localStorage.setItem('departamentosData', JSON.stringify(departamentos));
-    } else if (modo === 'actualizar') {
-      const departamentos = JSON.parse(localStorage.getItem('departamentosData')) || [];
+    } else if (modo === 'actualizar' && formData.id !== null) {
+      const departamentos = JSON.parse(localStorage.getItem('departamentosData'));
       const indexDepartamento = departamentos.findIndex((dep) => dep.id === formData.id);
       if (indexDepartamento !== -1) {
         departamentos[indexDepartamento] = formData;
         localStorage.setItem('departamentosData', JSON.stringify(departamentos));
       }
     }
-    window.location.href = '/admin/departamentos';
+    window.location.reload();
+  };
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0]; // Obtener el primer archivo seleccionado
+    if (file) {
+      const reader = new FileReader(); // Instanciar FileReader
+      reader.onload = () => {
+        setFormData((prevFormData) => ({
+          ...prevFormData,
+          image: reader.result, // Establecer el resultado de la lectura como el valor de la imagen
+        }));
+      };
+      reader.readAsDataURL(file); // Leer el archivo como un URL
+    }
+
+    // const file = event.target.files[0];
+
+  };
+
+  const getStartAdornment = (campo) => {
+    switch (campo) {
+      case 'name':
+        return <AccountCircle />;
+      case 'description':
+        return <Description />;
+      case 'phone':
+        return <Phone />;
+      case 'email':
+        return <Email />;
+      case 'image':
+        return <Image />;
+      case 'services':
+        return <RoomService />;
+      default:
+        return null;
+    }
+  };
+
+  const renderServiceInputs = () => {
+    return (
+      <div>
+        {formData.services.map((service, index) => (
+          <Grid item xs={12} key={index}>
+            <TextField
+              fullWidth
+              label={`Service ${index + 1}`}
+              name={`service-${index}`}
+              variant="outlined"
+              helperText={index === 0 ? 'Agrega un servicio' : null}
+              required
+              value={service}
+              onChange={(event) => handleServiceChange(event, index)}
+              InputProps={{
+                startAdornment: <AttractionsOutlined />,
+                endAdornment: (
+                  <Button
+                    variant="outlined"
+                    color="secondary"
+                    onClick={() => handleRemoveService(index)}
+                  >
+                    Remove
+                  </Button>
+                ),
+              }}
+              sx={{ mt: 2 }}
+            />
+          </Grid>
+        ))}
+        <Button
+          variant="outlined"
+          color="primary"
+          onClick={handleAddService}
+          startIcon={<AttractionsOutlined />}
+        >
+          Agregar otro servicio
+        </Button>
+      </div>
+    );
+  };
+
+  const renderInputField = (campo) => {
+    if (campo === 'services' && formData[campo]) {
+      return renderServiceInputs();
+    } else if (campo === 'image') {
+      return (
+        <TextField
+        fullWidth
+        label={campo.charAt(0).toUpperCase() + campo.slice(1)}
+        name={campo}
+        variant="outlined"
+      
+        onChange={(event) => handleFileChange(event)}
+        InputProps={{
+          startAdornment: getStartAdornment(campo),
+        }}
+        sx={{ mt: 2 }}
+        type="file"
+        inputProps={{ accept: 'image/*' }} // Asegúrate de permitir solo archivos de imagen
+        helperText="Solo se aceptan imágenes"
+      />
+      );
+    } else if (campo === 'state') {
+      return (
+        <TextField
+          fullWidth
+          label={campo.charAt(0).toUpperCase() + campo.slice(1)}
+          name={campo}
+          variant="outlined"
+          required
+          value={formData[campo]}
+          onChange={handleChange}
+          InputProps={{
+            startAdornment: getStartAdornment(campo),
+          }}
+          sx={{ mt: 2 }}
+          type="list"
+          disabled={true}
+          helperText="No se puede modificar, solo habilitar o deshabilitar. Por el momento, está habilitado"
+        />
+      );
+    } else {
+      return (
+        <TextField
+          fullWidth
+          label={campo.charAt(0).toUpperCase() + campo.slice(1)}
+          name={campo}
+          variant="outlined"
+          required
+          value={formData[campo]}
+          onChange={handleChange}
+          InputProps={{
+            startAdornment: getStartAdornment(campo),
+          }}
+          sx={{ mt: 2 }}
+          helperText="Ingrese los datos solicitados"
+        />
+      );
+    }
   };
 
   return (
@@ -113,24 +259,13 @@ const FormDepartamentos = ({ action, data }) => {
         <Grid container spacing={2}>
           {Object.keys(formData).map((campo) => (
             <Grid item xs={12} sm={6} key={campo}>
-              <TextField
-                fullWidth
-                label={campo.charAt(0).toUpperCase() + campo.slice(1)}
-                name={campo}
-                variant="outlined"
-                required
-                value={formData[campo]}
-                onChange={handleChange}
-                InputProps={{
-                  startAdornment: getStartAdornment(campo),
-                }}
-                sx={{ mt: 2 }}
-              />
+              {renderInputField(campo)}
+              <br />
             </Grid>
           ))}
           <Grid item xs={12}>
-            <Button type="submit" variant="contained" color="primary">
-              {modo === 'registrar' ? 'Registrar' : 'Actualizar'}
+            <Button variant="contained" color="primary" type="submit">
+              {modo === 'registrar' ? 'Registrar Departamento' : 'Actualizar Departamento'}
             </Button>
           </Grid>
         </Grid>
